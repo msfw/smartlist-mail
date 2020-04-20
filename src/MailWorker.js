9a2a5bf097b586340f59f7550dbb647790ed8a13
Andrei Franz Salvador <andrei.salvador@lincros.com>
2020-04-20 20:45:09 -0300

    Tema email

--- src/MailWorker.js
+++ src/MailWorker.js
@@ -1,7 +1,7 @@
 const { resolve } = require('path')
 require('dotenv').config({ path: resolve(__dirname, '../.env') })
 const rabbitSubscriber = require('./modules/rabbit/subcriber/SubscriberBase')
-const mailer = require('./modules/mailer')
+const send = require('./modules/mailer')
 const emailQueue = require('./config/queue')
 
 rabbitSubscriber([emailQueue], (message) => {
@@ -9,12 +9,5 @@ rabbitSubscriber([emailQueue], (message) => {
     const messageObject = JSON.parse(message)
 
     if (message !== '')
-        mailer.sendMail({
-            from: 'noreply.smartlist@gmail.com',
-            to: messageObject.to,
-            subject: messageObject.subject,
-            template: messageObject.template,
-            context: messageObject.context
-        }).catch(err => console.log(err))
-    
+        send(messageObject)    
 })
--- src/modules/mailer.js
+++ src/modules/mailer.js
@@ -1,7 +1,8 @@
 const nodemailer = require('nodemailer')
 const mailConfig = require('../config/mail')
 const hbs = require('nodemailer-express-handlebars')
-const exphbs = require ('express-handlebars');
+const exphbs = require('express-handlebars');
+const EmailTemplate = require('email-templates').EmailTemplate
 const path = require('path')
 
 const transporter = nodemailer.createTransport({
@@ -17,19 +18,34 @@ const transporter = nodemailer.createTransport({
     }
 });
 
-const templateDirectory = path.resolve(__dirname, '../templates')
+module.exports = (mail) => {
+    const sendMail = transporter.templateSender(new EmailTemplate(path.resolve(__dirname, `../templates/${mail.template}`)),
+     { from: 'noreply.smartlist@gmail.com' })
 
-transporter.use('compile', hbs({
-    viewEngine: exphbs.create({
-        layoutsDir: templateDirectory,
-        partialsDir: templateDirectory,
-        defaultLayout: 'default',
-        extname: '.hbs'    
-    }),
-    viewPath: templateDirectory,
-    extName: '.hbs'
-}));
+    sendMail({
+        to: mail.to,
+        subject: mail.subject
+    }, mail.context, (err) => {
+        if (err)
+            console.log("ERRO", err)
+        else
+            console.log("SENT")
+    })
+}
+
+// const templateDirectory = path.resolve(__dirname, '../templates/')
+
+// transporter.use('compile', hbs({
+//     viewEngine: exphbs.create({
+//         layoutsDir: templateDirectory,
+//         partialsDir: templateDirectory,
+//         defaultLayout: 'default',
+//         extname: '.hbs'
+//     }),
+//     viewPath: templateDirectory,
+//     extName: '.hbs'
+// }));
 
 // aplicar template depois.
 
-module.exports = transporter;
+// module.exports = transporter;
